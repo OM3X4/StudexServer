d838b0731790fb6a6ecb536295e9df716c5b6c5e
OM3X4 <om3x4e@gmail.com>
2025-05-04 21:54:41 +0300

    Add heatmap to the user serializer

--- core/serializers.py
+++ core/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Session , Subject , Topic , Tag , Goal
 from django.contrib.auth.models import User
 from datetime import date
+from .models import Session
+from django.db.models import Sum
 
 
 class SessionSerializer(serializers.ModelSerializer):
@@ -13,9 +15,6 @@ class SessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Session
         fields = ['id' ,  'subject' , 'subject_name' ,'topic' , 'topic_name' , 'tag' , 'tag_name' , 'focus' , 'duration']
-
-
-
 
 class TopicSerializer(serializers.ModelSerializer):
     # subject_name = serializers.CharField(source='subject.subject', read_only=True)
@@ -46,7 +45,10 @@ class GoalSerializer(serializers.ModelSerializer):
         model = Goal
         fields = ['id', 'end' , 'minutes' , 'subject' , 'topic' , 'subject_name' , 'topic_name' ]
 
-
+class HeatMapEntitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Session
+        fields = ['creation' , 'duration']
 
 class UserSerializer(serializers.ModelSerializer):
     subjects = SubjectSerializer(many=True, read_only=True)
@@ -55,10 +57,23 @@ class UserSerializer(serializers.ModelSerializer):
     sessions = SessionSerializer(many=True , read_only=True)
 
     studied_today = serializers.SerializerMethodField()
+    studied_this_week = serializers.SerializerMethodField()
+    studied_all_time = serializers.SerializerMethodField()
+
+    heatmap = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'subjects' , 'sessions' , 'goals' ,'tags']
+        fields = ['id', 'username', 'email', 'subjects' , 'sessions' , 'goals' ,'tags' , 'studied_today' , 'studied_this_week' , 'studied_all_time' , 'heatmap']
 
-    def get_studied_today(self , obj):
-        return obj.sessions.filter(creation__date=today).count()
+    def get_studied_today(self, obj):
+        return Session.objects.filter(user=obj, creation__date=date.today()).aggregate(total_minutes=Sum('duration'))['total_minutes'] or 0
+
+    def get_studied_this_week(self, obj):
+        return Session.objects.filter(user=obj, creation__week=date.today().isocalendar()[1]).aggregate(total_minutes=Sum('duration'))['total_minutes'] or 0
+
+    def get_studied_all_time(self, obj):
+        return Session.objects.filter(user=obj).aggregate(total_minutes=Sum('duration'))['total_minutes'] or 0
+
+    def get_heatmap(self , obj):
+        return HeatMapEntitySerializer(Session.objects.filter(user=obj).order_by('creation') , many=True).data
